3b52f622b33a82f616f97fc73c01bb1d7b625098
N3tCode <codyhub@gmail.com>
2022-12-27 16:31:04 -0500

    Reconfigure gridGen function and add working size buttons

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const gridGenerator = document.querySelector('.grid');
+const remove = document.querySelector('.gridGen')
 
-function gridGen(divs) {
+function gridGen(divs, size) {
+    while(gridGenerator.hasChildNodes()) {
+        gridGenerator.removeChild(gridGenerator.firstChild);
+      }
     for(let i = 0; i < divs; i++) {
         let createDiv = document.createElement('div');
-        createDiv.classList.add('gridGen');
+        createDiv.classList.add('gridGen' + size);
         createDiv.addEventListener('mouseenter', (e) => {
             createDiv.setAttribute('id', 'gridHover');
         })
@@ -14,4 +18,23 @@ function gridGen(divs) {
     }
 }
 
-gridGen(256);
+const button16 = document.querySelector('#but16');
+
+button16.addEventListener('click', () => {
+    gridGenerator.setAttribute('style', 'grid-template-columns: repeat(16, 1fr);'); 
+    gridGen(256, 16);
+})
+
+const button32 = document.querySelector('#but32');
+
+button32.addEventListener('click', () => {
+    gridGenerator.setAttribute('style', 'grid-template-columns: repeat(32, 1fr);'); 
+    gridGen(1024, 32);
+})
+
+const button64 = document.querySelector('#but64');
+
+button64.addEventListener('click', () => {
+    gridGenerator.setAttribute('style', 'grid-template-columns: repeat(64, 1fr);'); 
+    gridGen(4096, 64);
+})
